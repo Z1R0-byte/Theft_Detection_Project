f462ae8f7c20c64310dfb7485e4f8927935e9b34
Eeshaan Dutta <83598773+Z1R0-byte@users.noreply.github.com>
2022-07-04 17:54:18 +0530

    updated Models

---
+++ ModelSTAE.py
@@ -0,0 +1,34 @@
+from tensorflow import keras
+from keras.layers import Input,Conv3D,ConvLSTM2D,Conv3DTranspose
+from keras.models import Sequential, Model
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.utils import img_to_array,load_img
+# import imutils
+
+class STAE(Model):
+  def __init__(self):
+    super(STAE, self).__init__()
+    self.encoder = keras.Sequential([
+      Input(shape=(227, 227, 10, 1)),
+      Conv3D(filters= 128, kernel_size= (11, 11, 1), strides= (4, 4, 1), padding='valid', activation='relu'),
+      Conv3D(filters=64, kernel_size=(5, 5, 1), strides=(2, 2, 1), padding='valid', activation='relu')])
+    
+    self.bottleneck = keras.Sequential([
+      ConvLSTM2D(filters=64, kernel_size=(3, 3), strides=1, padding='same', dropout=0.4, recurrent_dropout=0.3, return_sequences=True),
+      ConvLSTM2D(filters=32, kernel_size=(3, 3), strides=1, padding='same', dropout=0.3, return_sequences=True),
+      ConvLSTM2D(filters=64, kernel_size=(3, 3), strides=1, return_sequences=True, padding='same', dropout=0.5)])
+    
+    self.decoder = keras.Sequential([
+      Conv3DTranspose(filters=128,kernel_size=(5,5,1),strides=(2,2,1),padding='valid',activation='relu'),
+      Conv3DTranspose(filters=1,kernel_size=(11,11,1),strides=(4,4,1),padding='valid',activation='relu')])
+
+  def call(self, x):
+    encoded = self.encoder(x)
+    reduced = self.bottleneck(encoded) 
+    decoded = self.decoder(reduced)
+    return decoded
+
+def loadModel():
+    model = STAE() 
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+    return model
